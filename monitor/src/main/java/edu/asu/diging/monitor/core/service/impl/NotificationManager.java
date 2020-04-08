79babe1c19b36d1c4d324f7982e2c74ff9be5238
aakash <aakash994@gmail.com>
2020-04-08 01:33:34 -0700

    [MON-12]PR changes and indentation

--- monitor/src/main/java/edu/asu/diging/monitor/core/service/impl/NotificationManager.java
+++ monitor/src/main/java/edu/asu/diging/monitor/core/service/impl/NotificationManager.java
@@ -24,69 +24,65 @@ import edu.asu.diging.monitor.core.service.INotificationManager;
 @Service
 public class NotificationManager implements INotificationManager {
 
-	private final Logger logger = LoggerFactory.getLogger(getClass());
+    private final Logger logger = LoggerFactory.getLogger(getClass());
 
-	@Autowired
-	private INotificationRecipientDbConnection dbConnection;
+    @Autowired
+    private INotificationRecipientDbConnection dbConnection;
 
-	@Autowired
-	private IEmailNotificationManager emailManager;
-	
-	@Autowired
-	private IAppManager appManager;
+    @Autowired
+    private IEmailNotificationManager emailManager;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * edu.asu.diging.monitor.core.service.impl.INotificationManager#addRecipient(
-	 * java.lang.String, java.lang.String)
-	 */
-	@Override
-	public boolean addRecipient(String name, String email, List<String> appIds) throws EmailAlreadyRegisteredException {
-		if (email == null || email.trim().isEmpty()) {
-			return false;
-		}
-		if (dbConnection.getById(email) != null) {
-			throw new EmailAlreadyRegisteredException();
-		}
-		INotificationRecipient recipient = new NotificationRecipient();
-		List<App> apps = new ArrayList<>();
-		recipient.setName(name);
-		recipient.setEmail(email);
-		recipient.setApps(apps);
-        for (String id: appIds) {
-            apps.add((App)appManager.getApp(id));
+    @Autowired
+    private IAppManager appManager;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * edu.asu.diging.monitor.core.service.impl.INotificationManager#addRecipient(
+     * java.lang.String, java.lang.String)
+     */
+    @Override
+    public boolean addRecipient(String name, String email, List<String> appIds) throws EmailAlreadyRegisteredException {
+        if (email == null || email.trim().isEmpty()) {
+            return false;
         }
-		try {
-			dbConnection.store(recipient);
-		} catch (UnstorableObjectException e) {
-			// should not happen
-			logger.error("Could not store recipient.", e);
-		}
-		return true;
-	}
+        if (dbConnection.getById(email) != null) {
+            throw new EmailAlreadyRegisteredException();
+        }
+        INotificationRecipient recipient = new NotificationRecipient();
+        recipient.setName(name);
+        recipient.setEmail(email);
+        recipient.setApps(new ArrayList<>());
+        for (String id : appIds) {
+            recipient.getApps().add((App) appManager.getApp(id));
+        }
+        try {
+            dbConnection.store(recipient);
+        } catch (UnstorableObjectException e) {
+            // should not happen
+            logger.error("Could not store recipient.", e);
+        }
+        return true;
+    }
 
-	@Override
-	public void deleteRecipient(String email) {
-		dbConnection.delete(email);
-	}
+    @Override
+    public void deleteRecipient(String email) {
+        dbConnection.delete(email);
+    }
 
-	@Override
-	public List<INotificationRecipient> getAllRecipients() {
-		return Arrays.asList(dbConnection.getAllRecipients());
-	}
+    @Override
+    public List<INotificationRecipient> getAllRecipients() {
+        return Arrays.asList(dbConnection.getAllRecipients());
+    }
 
-	@Override //change here
+    @Override
     public void sendNotificationEmails(IApp app, AppStatus previousStatus) {
-	    List<INotificationRecipient> recipients;
+        List<? extends INotificationRecipient> recipients;
         if (app.getRecipients().isEmpty()) {
             recipients = Arrays.asList(dbConnection.getAllRecipients());
-        }
-        else {
-            recipients = new ArrayList<>();
-            for (INotificationRecipient recipient : app.getRecipients())
-                recipients.add(recipient);
+        } else {
+            recipients = app.getRecipients();
         }
         for (INotificationRecipient recipient : recipients) {
             emailManager.sendAppStatusNotificationEmail(recipient.getEmail(), app, previousStatus.toString(),
