1fc7c9e9dbd0630c8af5258f1fc456e322d7486e
Aakash Kumar <aakash994@gmail.com>
2020-04-18 02:08:56 -0700

    [MON-12]Update app Function

--- monitor/src/main/java/edu/asu/diging/monitor/web/admin/ModifyAppController.java
+++ monitor/src/main/java/edu/asu/diging/monitor/web/admin/ModifyAppController.java
@@ -20,52 +20,35 @@ import edu.asu.diging.monitor.web.admin.forms.RecipientForm;
 
 @Controller
 public class ModifyAppController {
-	
-	
-	@Autowired
-	private IAppManager appManager;
-	
-	@Autowired
-	private IAppHelper appHelper;
-	
-	@Autowired
-    private INotificationManager manager;
 
-	
+    @Autowired
+    private IAppManager appManager;
+
+    @Autowired
+    private IAppHelper appHelper;
+
     @RequestMapping(value = "/admin/apps/{id}/modify", method = RequestMethod.GET)
     public String show(Model model, @PathVariable("id") String id) {
         AppForm appForm = new AppForm();
         IApp app = appManager.getApp(id);
-        appForm.setDescription(app.getDescription());
-        appForm.setExpectedReturnCodes(app.getExpectedReturnCodes());
-        appForm.setHealthUrl(app.getHealthUrl());
-        appForm.setId(app.getId());
-        appForm.setMethod(app.getMethod());
-        appForm.setName(app.getName());
-        appForm.setPingInterval(app.getPingInterval());
-        appForm.setRecipients(manager.getAllRecipients().stream().map(r -> {
-            RecipientForm recipientForm = new RecipientForm();
-            recipientForm.setName(r.getName());
-            recipientForm.setEmail(r.getEmail());
-            return recipientForm;
-        }).collect(Collectors.toList()));
-        appForm.setRetries(app.getRetries());
-        appForm.setTimeout(app.getTimeout());
-        appForm.setWarningReturnCodes(app.getWarningReturnCodes());
+        appHelper.copyAppInfoToForm(app, appForm);
         model.addAttribute("appForm", appForm);
         model.addAttribute("appRecipients",
                 app.getRecipients().stream().map(x -> x.getEmail()).collect(Collectors.toList()));
         return "admin/apps/show";
     }
 
-	@RequestMapping(value="/admin/apps/{id}/modify", method=RequestMethod.POST)
-	public String update(@ModelAttribute AppForm appForm, @PathVariable("id") String id, RedirectAttributes redirectAttrs) {
-		IApp app = appManager.getApp(id);
-		appHelper.copyAppInfo(app, appForm);
-		appManager.updateApp(app);
-		redirectAttrs.addFlashAttribute("show_alert", true);
-		redirectAttrs.addFlashAttribute("alert_type", "success");
-		redirectAttrs.addFlashAttribute("alert_msg", "App was successfully updated.");
-		return "redirect:/";
-	}
+    @RequestMapping(value = "/admin/apps/{id}/modify", method = RequestMethod.POST)
+    public String update(@ModelAttribute AppForm appForm, @PathVariable("id") String id,
+            RedirectAttributes redirectAttrs) {
+        IApp app = appManager.getApp(id);
+        if (app.getRecipients() != null)
+            appManager.deleteExistingRecipients(app);
+        app = appHelper.copyAppInfo(appManager.getApp(id), appForm);
+        appManager.updateApp(app);
+        redirectAttrs.addFlashAttribute("show_alert", true);
+        redirectAttrs.addFlashAttribute("alert_type", "success");
+        redirectAttrs.addFlashAttribute("alert_msg", "App was successfully updated.");
+        return "redirect:/";
+    }
 }
