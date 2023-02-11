21a9611dd668a48b34cbb08679b344a6ce92d2dc
Sreechandra Allala <srichandraallala@gmail.com>
2023-02-10 17:28:14 -0700

    [MON-21] Added JUnits for NotificationManager.modify method

--- monitor/src/test/java/edu/asu/diging/monitor/core/service/impl/NotificationManagerTest.java
+++ monitor/src/test/java/edu/asu/diging/monitor/core/service/impl/NotificationManagerTest.java
@@ -1,5 +1,7 @@
 package edu.asu.diging.monitor.core.service.impl;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.junit.Assert;
@@ -11,9 +13,12 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import edu.asu.diging.monitor.core.db.INotificationRecipientDbConnection;
+import edu.asu.diging.monitor.core.exceptions.UnstorableObjectException;
 import edu.asu.diging.monitor.core.model.INotificationRecipient;
+import edu.asu.diging.monitor.core.model.impl.App;
 import edu.asu.diging.monitor.core.model.impl.NotificationRecipient;
 import edu.asu.diging.monitor.core.notify.impl.EmailNotificationManager;
+import edu.asu.diging.monitor.core.service.IAppManager;
 
 public class NotificationManagerTest {
 	
@@ -22,6 +27,9 @@ public class NotificationManagerTest {
 	
 	@Mock
 	private EmailNotificationManager emailManager;
+	
+	@Mock
+	private IAppManager appmanager;
 	
 	@InjectMocks
 	private NotificationManager managerToTest;
@@ -32,6 +40,12 @@ public class NotificationManagerTest {
 	private NotificationRecipient recipient2;
 	private String ID1 = "email@ID1";
 	private String ID2 = "email@ID2";
+	private String id1Name = "ID1";
+	private String id2Name = "ID2";
+	private String appId1 = "App1";
+	private String appId2 = "App2";
+	private App app1 = new App();
+	private App app2 = new App();
 	
 	
 	@Before
@@ -39,13 +53,23 @@ public class NotificationManagerTest {
 		MockitoAnnotations.initMocks(this);
 		recipient1 = new NotificationRecipient();
 		recipient1.setEmail(ID1);
+		recipient1.setName(id1Name);
+		
 		recipient2 = new NotificationRecipient();
 		recipient2.setEmail(ID2);
+		recipient2.setName(id2Name);
 		storedRecipients = new NotificationRecipient[] {recipient1, recipient2};
+		
+		app1.setId(appId1);
+		app1.setName(appId1);
+		app2.setId(appId2);
+		app2.setName(appId2);
 		
 		Mockito.when(dbConn.getAllRecipients()).thenReturn(storedRecipients);
 		Mockito.when(dbConn.getById(ID1)).thenReturn(recipient1);
 		Mockito.when(dbConn.getById(ID2)).thenReturn(recipient2);
+		Mockito.when(appmanager.getApp(appId1)).thenReturn(app1);
+        Mockito.when(appmanager.getApp(appId2)).thenReturn(app2);
 	}
 	
 	
@@ -62,6 +86,20 @@ public class NotificationManagerTest {
         managerToTest.deleteRecipient(ID1);
         Mockito.verify(dbConn).delete(ID1);
     }
+	
+	@Test
+	public void test_modifyRecipient_success() {
+	    List<String> appIds = new ArrayList<>();
+	    appIds.add(appId1);
+	    appIds.add(appId2);
+	    managerToTest.modifyRecipient(ID1, id1Name, appIds);
+	    try {
+            Mockito.verify(dbConn).update(Mockito.any());
+        } catch (UnstorableObjectException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+	}
 	
 
 }
